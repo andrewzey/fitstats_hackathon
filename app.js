4f646c66d5ae0ad02f92080341bf103a8aa53fd8
Andrew Zey <andrew@andrewzey.com>
2014-07-07 13:06:49 -0700

    Add dummy data for pre-database design

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var app = angular.module('fitStats', []);
 
 app.controller('DaySelect', function($scope, $http) {
-	$scope.currentDay = new Date();
+	$scope.currentDay = {};
+	$scope.currentDay.date = new Date();
+
+	//Dummy Data before populating firebase
+	$scope.currentDay.weight = 165.5;
+	$scope.currentDay.bf = 13.2;
+	$scope.currentDay.hr = 65;
+	$scope.currentDay.bps = 120;
+	$scope.currentDay.bpd = 75;
+	$scope.currentDay.calories = 2400;
+	$scope.currentDay.protein = 165;
+	$scope.currentDay.carbs = 240;
+	$scope.currentDay.fat = 87;
 });
