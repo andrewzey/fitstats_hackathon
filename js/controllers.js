f22750e85c17e817e44c872f0935e32bca9c8361
Andrew Zey <andrew@andrewzey.com>
2014-07-07 19:43:29 -0700

    Add food block

--- js/controllers.js
+++ js/controllers.js
@@ -77,3 +77,29 @@ app.controller('BPController', function($rootScope, $scope, FormFunctions){
   };
 
 });
+
+app.controller('FoodController', function($rootScope, $scope, FormFunctions){
+  $scope.inputMode = true;
+
+  if ($rootScope.currentDay.calories && $rootScope.currentDay.protein && $rootScope.currentDay.carbs && $rootScope.currentDay.fat) {
+    $scope.inputMode = false;
+  }
+  $scope.formData = {};
+  $scope.formData.bps = $rootScope.currentDay.bps;
+  $scope.formData.bpd = $rootScope.currentDay.bpd;
+
+  $scope.submit = FormFunctions.submit;
+
+  $scope.submitAll = function(){
+    $scope.submit('calories', 0);
+    $scope.submit('protein', 0);
+    $scope.submit('carbs', 0);
+    $scope.submit('fat', 0);
+  };
+
+  //should be refactored into separate factory
+  $scope.edit = function(){
+    $scope.inputMode = true;
+  };
+
+});
