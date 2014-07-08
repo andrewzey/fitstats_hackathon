361ba2fe190977d6c660e2609251dcc91c2bc11c
Andrew Zey <andrew@andrewzey.com>
2014-07-07 18:09:14 -0700

    Refactor controllers and factories into separate files

---
+++ js/controllers.js
@@ -0,0 +1,19 @@
+angular.module('controllers', [])
+
+app.controller('WeightController', function($rootScope, $scope, FormFunctions){
+  $scope.inputMode = true;
+
+  if ($rootScope.currentDay.weight) {
+    $scope.inputMode = false;
+  }
+  $scope.formData = {};
+  $scope.formData.weight = $rootScope.currentDay.weight;
+
+  $scope.submit = FormFunctions.submit;
+
+  //should be refactored into separate factory
+  $scope.edit = function(){
+    $scope.inputMode = true;
+  };
+
+});
---
+++ js/factories.js
@@ -0,0 +1,17 @@
+angular.module('factories', [])
+
+app.factory('FormFunctions', function($rootScope, $filter){
+  var submit = function(item) {
+    $rootScope.currentDay[item] = $filter("number")(this.formData[item], 1);
+    this.inputMode = false;
+  };
+
+  //for later refactor, put edit function
+  // var edit = function(){
+  // };
+
+  return {
+    submit: submit
+    //, edit: edit
+  };
+});
