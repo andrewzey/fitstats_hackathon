ac70a63bfc1ee388a016fcdee43fc55c43df3543
Andrew Zey <andrew@andrewzey.com>
2014-07-08 17:42:01 -0700

    Add placeholders for exercise and water

--- app/js/user/controllers.js
+++ app/js/user/controllers.js
@@ -10,11 +10,11 @@ app.controller('Main', function($rootScope, $scope){
   //Dummy Data before populating firebase
   $rootScope.currentDay = {};
   $rootScope.currentDay.date = new Date();
-  // $rootScope.currentDay.weight = 165.5;
-  // $rootScope.currentDay.bf = 13.2;
-  // $rootScope.currentDay.hr = 65;
-  // $rootScope.currentDay.bps = 120;
-  // $rootScope.currentDay.bpd = 75;
+  $rootScope.currentDay.weight = 165.5;
+  $rootScope.currentDay.bf = 13.2;
+  $rootScope.currentDay.hr = 65;
+  $rootScope.currentDay.bps = 120;
+  $rootScope.currentDay.bpd = 75;
   $rootScope.currentDay.calories = 2400;
   $rootScope.currentDay.protein = 165;
   $rootScope.currentDay.carbs = 240;
